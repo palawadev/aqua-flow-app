961ed8c7a2bccf040b943509332d93eb6769b79b
ahadanfauzan123 <ahadanfauzan123@gmail.com>
2024-07-26 15:32:05 +0700

    backend 5

--- src/app/(root)/(admin)/Dashboard/layout.tsx
+++ src/app/(root)/(admin)/Dashboard/layout.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import Navbar from "./NavbarDashboard/Navbar";
+
+export const metadata: Metadata = {
+  title: "Water Management System",
+  description: "BARMA Technology Water Management System",
+  icons: {icon: "/logo/Logowhite.png"}
+};
 
 export default function DashboardLayout({
   children,
